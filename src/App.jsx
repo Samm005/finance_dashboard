acfefdea645bc184b4ed54db7b01994e1dc51e2a
Samm005 <samudyathakaranth@gmail.com>
2026-04-02 23:14:49 +0530

    clean project structure, add assets and firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
     <>
-     <h1>Hello world</h1>
+     <div><Header/></div>
     </>
   )
 }
