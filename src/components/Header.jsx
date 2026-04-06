05f91814810915dc637adac4ae9b48f187d86d8c
Samm005 <samudyathakaranth@gmail.com>
2026-04-06 15:40:48 +0530

    Signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React, { use, useEffect } from 'react'
-import './Header.css'
-import {auth} from '../firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import React, { use, useEffect } from "react";
+import "./Header.css";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { toast } from "react-toastify";
 
 function Header() {
-
-  const [user,loading]=useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,16 +17,32 @@ function Header() {
   }, [user, loading]);
 
   function handleLogout() {
-    alert('Logged out');
+    try {
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          toast.success("Logged out successfully");
+          navigate("/");
+        })
+        .catch((error) => {
+          toast.error(error.message);
+          // An error happened.
+        });
+    } catch (error) {
+      toast.error(error.message);
+    }
   }
 
-
   return (
-    <div className='navbar'>
-      <p className='logo'>WealthTrack</p>
-      {user && <p className='logout' onClick={handleLogout}>Logout</p>}
+    <div className="navbar">
+      <p className="logo">WealthTrack</p>
+      {user && (
+        <p className="logo link" onClick={handleLogout}>
+          Logout
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
